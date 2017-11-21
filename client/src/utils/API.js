aba18dac0649c25713eff732c170face2ee51410
Hernan Tejada <hernan.tejada@gmail.com>
2017-11-20 20:16:02 -0500

    Changing API's https back to original

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,7 +6,7 @@ const urlx = "season=" + seasonx + "&week=" + weekx + "&format=json";
 
 export default {
   getPlayersList: function() {
-    return axios.get("https://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&" + urlx);
+    return axios.get("http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&" + urlx);
   },
   getSportsFeeds: function() {
     return axios.get("https://api.mysportsfeeds.com/v1.1/pull/nfl/" + seasonx + "-regular/cumulative_player_stats.json", {
